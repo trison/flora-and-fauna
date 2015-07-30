5ffb22b1f9dbb0a0106fde26970536a6634f81d7
Trison Nguyen <trison.ng@gmail.com>
2015-07-30 10:00:18 -0700

    added title fade in

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
+// $(document).ready(function(){
+
+//     // Fill in the blanks!
+//     $('title').animate({ top: '+=100px'}, 1000 );
+// });
+
+// function myFunction() {
+//     document.getElementById("demo").innerHTML = "Paragraph changed.";
+//     }
+
 $(document).ready(function(){
 
-    // Fill in the blanks!
-    $('title').animate({ top: '+=100px'}, 1000 );
+    $("#button").click( function(){
+    	$('#demo').slideUp();
+    });
+
+    $('#flora_and_fauna').hide().delay(100).fadeIn(800);
+    $('#collective').hide().delay(700).fadeIn(850);
+
+
 });
