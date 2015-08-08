6d41f1343ecb36ffaad4ff476c0fb8fd54c7e7b3
Trison Nguyen <trison.ng@gmail.com>
2015-08-08 01:23:59 -0700

    jquery functionality on menu and title transitions complete

--- js/script.js
+++ js/script.js
@@ -14,17 +14,32 @@ $(document).ready(function(){
     	$('#demo').slideUp();
     });
 
+    $('#name').hide().delay(150).fadeToggle(800);
 
-    $('#name').hide().delay(150).fadeIn(800);
+    var scroll_pos = 0;
+    var menu_changed = false;
+    var $header = $('.header');
+    var $ul_li_a = $('.ul li a');
 
-     $('body').bind('mousewheel', function(e){
-     if(e.originalEvent.wheelDelta < 0) {
-         //scroll down
-         console.log('Down');
-     }else {
-         //scroll up
-         console.log('Up');
-     }
- });
 
+    //change menu style once scrolled down enough
+    $('body').on('mousewheel', function(){
+    	scroll_pos = $(this).scrollTop();
+    	console.log(scroll_pos);
+    	if(scroll_pos >= 200 && !menu_changed){
+    		$header.css({'background-color': 'gray'});
+    		//$header.css("opacity", "0.8");
+    		$ul_li_a.css({'color': 'black'});
+    		$('#name').fadeToggle(350);
+    		menu_changed=true;
+    	}
+    	else if(menu_changed && scroll_pos < 200){
+    		console.log("here");
+    		$('#name').delay(150).fadeIn(800);
+    		$header.css({'background-color': ''});
+    	//	$header.css("opacity", "1");
+    		
+    		menu_changed=false;
+    	}
+    });
 });
