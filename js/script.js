cf0a47a9c551151ecfa24f3a33eb3fbb23b2bedc
Trison Nguyen <trison.ng@gmail.com>
2015-08-09 15:48:04 -0700

    further front page design, added some SEO

--- js/script.js
+++ js/script.js
@@ -1,13 +1,3 @@
-// $(document).ready(function(){
-
-//     // Fill in the blanks!
-//     $('title').animate({ top: '+=100px'}, 1000 );
-// });
-
-// function myFunction() {
-//     document.getElementById("demo").innerHTML = "Paragraph changed.";
-//     }
-
 $(document).ready(function(){
 
 	var scroll_pos = 0;
@@ -16,15 +6,22 @@ $(document).ready(function(){
     var $pic1 = $('.pic1');
     var $pic2 = $('.pic2');
     var $pic3 = $('.pic3');
-
+    var $social = $('.social');
+ 
     $('#line').hide().delay(150).toggle(800);
     $('#intro').hide().delay(100).fadeToggle(800);
     $header.hide().delay(200).fadeToggle(800);
+
+ 	$social.hover(
+    	function(){$(this).css({'color': 'orange'})},
+    	function(){$(this).css({'color': 'white'})}
+    );
 
     //change styles during scrolling
     $('body').on('mousewheel', function(){
     	scroll_pos = $(this).scrollTop();
     	console.log(scroll_pos);
+    	
     	//hide intro text
     	if(scroll_pos >= 250 && !title_changed){
     		$('#intro').fadeToggle(350);
@@ -32,12 +29,12 @@ $(document).ready(function(){
     		title_changed=true;
     	}
     	//colour header background
-    	else if(scroll_pos >= 625 && !menu_changed){
+    	else if(scroll_pos >= 725 && !menu_changed){
 			$header.css({'background-color': 'gray'});
     		menu_changed=true;
     	}
     	//clear header background
-    	else if(menu_changed && scroll_pos < 625){
+    	else if(menu_changed && scroll_pos < 725){
     		$header.css({'background-color': ''});
     		menu_changed=false;
     	}
@@ -50,14 +47,14 @@ $(document).ready(function(){
     	//fade in pictures
     	else if(scroll_pos > 400){
     		$pic1.css({'opacity': '1'});
-    		$pic2.delay(150).queue(function (next) { 
+    		$pic2.delay(200).queue(function (next) { 
 			    $(this).css({'opacity': '1'}); 
 			    next(); 
-			  });
-    		$pic3.delay(200).queue(function (next) { 
+			 });
+    		$pic3.delay(300).queue(function (next) { 
 			    $(this).css({'opacity': '1'}); 
 			    next(); 
-			  });
+			 });
     	}
     });
 });
